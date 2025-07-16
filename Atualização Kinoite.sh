560ab202ac501f20dfdbcb043eaaf466900abb8b
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2025-07-16 16:54:54 -0300

    Add files via upload

--- Atualização Kinoite.sh
+++ Atualização Kinoite.sh
@@ -22,19 +22,22 @@ ostree remote refs fedora;
 ##Atualização do sistema##
 rpm-ostree upgrade;
 
-##Instalação de programas##
-#rpm-ostree install fastfetch; rpm-ostree install simple-scan; rpm-ostree install lshw-gui; rpm-ostree install java-latest-openjdk; rpm-ostree install btop; rpm-ostree install kitty; rpm-ostree install gnome-boxes; rpm-ostree install gnome-disk-utility; rpm-ostree install lsd; rpm-ostree install emacs; rpm-ostree install krusader; rpm-ostree install thunderbird; rpm-ostree install kcalc; rpm-ostree install krename; rpm-ostree install kget; rpm-ostree install transmission; rpm-ostree install foomatic;
-
-##Atualização de versão##
-#rpm-ostree rebase fedora:fedora/40/x86_64/kinoite;
+##Instalação dos programas Nativos##
+#rpm-ostree install fastfetch; rpm-ostree install lshw-gui; rpm-ostree install java-latest-openjdk; rpm-ostree install gnome-disk-utility; rpm-ostree install lsd; rpm-ostree install foomatic;
 
 ##Instalação dos programas Flatpak##
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y;  flatpak install flathub ar.com.tuxguitar.TuxGuitar -y;
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub org.gnome.SimpleScan -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub dev.geopjr.Collision -y; flatpak install flathub org.gnome.Boxes -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub io.github.JaGoLi.ytdl_gui -y; flatpak install flathub org.audacityteam.Audacity -y; flatpak install flathub me.kozec.syncthingtk -y; flatpak install flathub com.anydesk.Anydesk -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub io.qt.QtCreator -y; flatpak install flathub io.qt.Designer -y; flatpak install flathub io.qt.qdbusviewer -y; flatpak install flathub io.qt.Linguist -y;
 
 ##Atualização do Flatpak##
 flatpak update -y;
 
-  ###Universal Blue###
+##Atualização de versão##
+#rpm-ostree rebase fedora:fedora/42/x86_64/kinoite;
+#rpm-ostree rebase fedora:fedora/42/x86_64/cosmic-atomic;
+#rpm-ostree rebase fedora:fedora/42/x86_64/onyx;
+#rpm-ostree rebase fedora:fedora/42/x86_64/silverblue;
+
+    ###Universal Blue###
   ##Comandos para troca de interface pelo Kinoite##
 ##Rebase XFCE
 #rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/vauxite-main:latest;
@@ -54,8 +57,6 @@ flatpak update -y;
 ##Rebase Gnome OS NVIDIA##
 #rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/beyond-nvidia:latest;
 
-systemctl reboot
-
 #while true; do
 #    read -p "Deseja reiniciar o computador agora? [Yy/Nn]: " yn
 #    case "$yn" in
@@ -64,3 +65,5 @@ systemctl reboot
 #        * ) echo "Por favor, responda com 'Y' ou 'N'.";;
 #    esac
 #done
+
+systemctl reboot
