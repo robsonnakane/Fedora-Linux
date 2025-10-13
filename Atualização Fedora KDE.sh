22b7fb694c4f57adfab67f0cb8d8c9197dc4b5f8
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2025-10-13 17:35:27 -0300

    Add files via upload

--- Atualização Fedora KDE.sh
+++ Atualização Fedora KDE.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-###Fedora Workstation###
+###Fedora Server KDE###
 ###Atualização completa do sistema###
 
 ##Execução do arquivo de atualização no terminal##
-#/home/robsonnakane/Documentos/'Atualização Fedora KDE.sh'#
+#/home/robsonnakane/Documentos/'Atualização Fedora Server KDE.sh'#
 
 #Edição do arquivo no terminal#
-#nano /home/robsonnakane/Documentos/'Atualização Fedora KDE.sh'#
+#nano /home/robsonnakane/Documentos/'Atualização Fedora Server KDE.sh'#
 
-sudo dnf autoremove -y; ##remova todos os pacotes desnecessários que foram originalmente instalados como dependências
-sudo dnf makecache -y; ##gera o cache de metadados
-sudo dnf check -y; ##verifica se há problemas no packagedb
-sudo dnf check-update -y; ##verifica se há atualizações de pacote disponíveis
-sudo dnf upgrade --refresh -y; ##atualiza um ou mais pacotes em seu sistema
-sudo dnf distro-sync -y; ##sincroniza os pacotes instalados com as últimas versões disponíveis
+sudo dnf5 autoremove -y; ##remova todos os pacotes desnecessários que foram originalmente instalados como dependências
+sudo dnf5 makecache -y; ##gera o cache de metadados
+sudo dnf5 check -y; ##verifica se há problemas no packagedb
+sudo dnf5 check-update -y; ##verifica se há atualizações de pacote disponíveis
+sudo dnf5 upgrade --refresh -y; ##atualiza um ou mais pacotes em seu sistema
+sudo dnf5 distro-sync -y; ##sincroniza os pacotes instalados com as últimas versões disponíveis
+sudo dnf5 system-upgrade -y; #atualiza o sistema para a versão mais nova
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg; ##Atualiza o grub
 
+##Desativação de serviços para computadores fracos
+#sudo systemctl stop auditd; sudo systemctl disable auditd; sudo systemctl stop systemd-oomd systemd-oomd.socket; sudo systemctl disable systemd-oomd systemd-oomd.socket; sudo systemctl stop irqbalance; sudo systemctl disable irqbalance; sudo systemctl stop fprintd; sudo systemctl disable fprintd; sudo systemctl stop bluetooth; sudo systemctl disable bluetooth; sudo systemctl stop cups; sudo systemctl disable cups; sudo systemctl stop avahi-daemon; sudo systemctl disable avahi-daemon;
+
 ##instalação dos programas
-#sudo dnf install fastfetch -y; sudo dnf install foomatic -y; sudo dnf install simple-scan -y; sudo dnf install java-latest-openjdk -y; sudo dnf install btop -y; sudo dnf install kitty -y; sudo dnf install gnome-disks -y; sudo dnf install boxes -y;
+#sudo dnf install fastfetch -y; sudo dnf install foomatic -y; sudo dnf install java-latest-openjdk -y; sudo dnf install  gnome-disk-utility -y; sudo dnf install lshw-gui -y; sudo dnf install lsd -y; sudo dnf install rsync -y;
 
 ##Instalação dos programas Flatpak##
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
-#flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.gnome.gedit -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y;  flatpak install flathub ar.com.tuxguitar.TuxGuitar -y; flatpak install flathub com.anydesk.Anydesk -y;
+#sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
+#flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub org.gnome.SimpleScan -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub dev.geopjr.Collision -y; flatpak install flathub org.gnome.Boxes -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.videolan.VLC -y; flatpak run me.aandrew.ytdownloader -y; flatpak install flathub org.audacityteam.Audacity -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub org.gimp.GIMP -y; flatpak install flathub org.mozilla.firefox -y; flatpak install flathub org.kde.kate -y; flatpak install flathub io.github.prateekmedia.appimagepool -y; flatpak install flathub org.kde.kget -y;
 
 ##Atualização do Flatpak##
 flatpak update -y;
+
+#Não há a necessidade de alterar o Firewall de nenhuma máquina. Apenas ativar o sshd.
+#Do Notebook para Desktop
+#sudo rsync -avzrp --delete /home/robsonnakane/ robsonnakane@192.168.15.110:/home/robsonnakane/
+#Do Desktop para Notebook
+#sudo rsync -avzrp --delete /home/robsonnakane/ robsonnakane@192.168.15.173:/home/robsonnakane/
+#Do Notebook para Netbook
+#sudo rsync -avzrp --delete /home/robsonnakane/ robsonnakane@192.168.15.15:/home/robsonnakane/lenovo/
 
 systemctl reboot
--- Atualização Kinoite by distrobox.sh
+++ Atualização Kinoite by distrobox.sh
@@ -3,49 +3,27 @@
 ###Fedora Kinoite by distrobox###
 ###Atualização completa do sistema###
 
-##Execução do arquivo de atualização no terminal##
-#/home/robsonnakane/Documentos/'Atualização Kinoite by distrobox.sh'#
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg; ##Atualiza o grub
+rpm-ostree cancel; ##Cancela a atualização automática dos repositórios##
+rpm-ostree cleanup -m; rpm-ostree cleanup -r; rpm-ostree cleanup -p; rpm-ostree cleanup -b; rpm-ostree override reset --all; ##Limpeza cache##
+rpm-ostree refresh-md;##Atualizção dos repositórios##
+ostree remote refs fedora;##Pesquisar a versão disponível##
+rpm-ostree upgrade --preview; ##Conferência dos pacotes##
+rpm-ostree upgrade; ##Atualização do sistema##
 
-#Edição do arquivo no terminal#
-#nano /home/robsonnakane/Documentos/'Atualização Kinoite by distrobox.sh'#
-
-##Atualiza o grub
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg;
-##Cancela a atualização automática dos repositórios##
-rpm-ostree cancel; 
-##Limpeza cache##
-rpm-ostree cleanup -m; rpm-ostree cleanup -r; rpm-ostree cleanup -p; rpm-ostree cleanup -b; rpm-ostree override reset --all;
-##Atualizção dos repositórios##
-rpm-ostree refresh-md;
-##Conferência dos pacotes##
-rpm-ostree upgrade --preview;
-##Pesquisar a versão disponível##
-ostree remote refs fedora;
-##Atualização do sistema##
-rpm-ostree upgrade;
-
-##Instalação de programas no distrobox archlinux##
-#curl -sL https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh; distrobox create --name archlinux --image archlinux:latest; distrobox enter archlinux -- sudo pacman -S --noconfirm fastfetch; distrobox enter archlinux -- sudo pacman -S --noconfirm simple-scan; distrobox enter archlinux -- sudo pacman -S --noconfirm kdenlive; distrobox enter archlinux -- sudo pacman -S --noconfirm inkscape; distrobox enter archlinux -- sudo pacman -S --noconfirm thunderbird; distrobox enter archlinux -- sudo pacman -S --noconfirm audacious; distrobox enter archlinux -- sudo pacman -S --noconfirm gnome-boxes; distrobox enter archlinux -- sudo pacman -S --noconfirm gimp; distrobox enter archlinux -- sudo pacman -S --noconfirm audacity; distrobox enter archlinux -- sudo pacman -S --noconfirm vlc; distrobox enter archlinux -- sudo pacman -S --noconfirm transmission-gtk; distrobox enter archlinux -- sudo pacman -S --noconfirm rpi-imager; distrobox enter archlinux -- sudo pacman -S --noconfirm firefox;
+##Instalação de pacotes no distrobox archlinux##
+#curl -sL https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh; distrobox create --name archlinux --image archlinux:latest; distrobox enter archlinux -- sudo pacman -Syyuu --noconfirm fastfetch simple-scan kdenlive inkscape thunderbird audacious gnome-boxes gimp audacity transmission-gtk rpi-imager firefox gwenview kate;
 
 ##Exportação dos pacotes instalados no distrobox##
-#distrobox enter archlinux -- distrobox-export --app simple-scan; distrobox enter archlinux -- distrobox-export --app kdenlive; distrobox enter archlinux -- distrobox-export --app inkscape; distrobox enter archlinux -- distrobox-export --app thunderbird; distrobox enter archlinux -- distrobox-export --app audacious; distrobox enter archlinux -- distrobox-export --app gnome-boxes; distrobox enter archlinux -- distrobox-export --app gimp; distrobox enter archlinux -- distrobox-export --app audacity; distrobox enter archlinux -- distrobox-export --app vlc; distrobox enter archlinux -- distrobox-export --app transmission-gtk; distrobox enter archlinux -- distrobox-export --app rpi-imager; distrobox enter archlinux -- distrobox-export --app firefox;
+#distrobox enter archlinux -- distrobox-export --app simple-scan; distrobox enter archlinux -- distrobox-export --app kdenlive; distrobox enter archlinux -- distrobox-export --app inkscape; distrobox enter archlinux -- distrobox-export --app thunderbird; distrobox enter archlinux -- distrobox-export --app audacious; distrobox enter archlinux -- distrobox-export --app gnome-boxes; distrobox enter archlinux -- distrobox-export --app gimp; distrobox enter archlinux -- distrobox-export --app audacity; distrobox enter archlinux -- distrobox-export --app transmission-gtk; distrobox enter archlinux -- distrobox-export --app rpi-imager; distrobox enter archlinux -- distrobox-export --app firefox; distrobox enter archlinux -- distrobox-export --app gwenview; distrobox enter archlinux -- distrobox-export --app kate;
 
-##Atualização dos pacotes archlinux instalados via distrobox##
-distrobox enter archlinux -- sudo pacman -Syyuu --noconfirm;
+##Instalação dos pacotes Flatpak##
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.spotify.Client -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub org.chromium.Chromium -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.kde.kget -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub net.mkiol.SpeechNote -y; flatpak install flathub com.warlordsoftwares.youtube-downloader-4ktube -y;
 
-##Instalação de programas no distrobox fedora##
-#distrobox create --name fedora --image fedora:latest; distrobox enter fedora -- sudo dnf install fastfetch -y;
+#rpm-ostree rebase fedora:fedora/43/x86_64/kinoite; ##Atualização de versão##
 
-##Atualização dos pacotes fedora instalados via distrobox##
-distrobox enter fedora -- sudo dnf5 autoremove -y; distrobox enter fedora -- sudo dnf5 makecache -y; distrobox enter fedora -- sudo dnf5 check -y; distrobox enter fedora -- sudo dnf5 check-update -y; distrobox enter fedora -- sudo dnf5 upgrade --refresh -y; distrobox enter fedora -- sudo dnf5 distro-sync -y;
+distrobox enter archlinux -- sudo pacman --noconfirm -Syyuu; ##Atualização Distrobox##
 
-##Atualização de versão##
-#rpm-ostree rebase fedora:fedora/43/x86_64/kinoite;
-
-##Instalação dos programas Flatpak##
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.spotify.Client -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; flatpak install flathub org.chromium.Chromium -y;
-
-##Atualização do Flatpak##
-flatpak update -y;
+flatpak update -y; ##Atualização do Flatpak##
 
 systemctl reboot
